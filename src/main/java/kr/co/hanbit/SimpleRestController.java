c32553be2466c195b1aa97a5e9a0cd2a46fd5581
E. K. Lee <wolf@teoal.net>
2025-11-07 00:21:29 +0900

    7장 실습 완료

--- src/main/java/kr/co/hanbit/SimpleRestController.java
+++ src/main/java/kr/co/hanbit/SimpleRestController.java
@@ -1,6 +1,7 @@
 package kr.co.hanbit;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,5 +14,15 @@ public class SimpleRestController {
     @RequestMapping("/bye")
     public String bye() {
         return "Bye~";
+    }
+
+    @RequestMapping("/article")
+    public String createArticle(@RequestParam("title") String title, @RequestParam("content") String content) {
+        return String.format("title=%s / content=%s", title, content);
+    }
+
+    @RequestMapping("/get-with-no-parameter")
+    public String getWithNoPrarameter() {
+        return "파라미터가 없는 GET 요청";
     }
 }
