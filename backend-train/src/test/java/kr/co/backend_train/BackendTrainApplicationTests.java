52c6fe49689ce85162adb4df529e9dc4f764275d
E. K. Lee <wolf@teoal.net>
2025-11-07 15:28:45 +0900

    Product Management 실습 시작

---
+++ backend-train/src/test/java/kr/co/backend_train/BackendTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.backend_train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
