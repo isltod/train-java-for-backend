297f3c4bec2511d3cafa63b4da1a1b2943bdfc99
E. K. Lee <wolf@teoal.net>
2025-11-07 14:09:17 +0900

    Reset to start

--- 03/HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld {
-    public static void main(String[] args) {
-        //시작...
-        System.out.println("헬로우 월드!!");
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# train-java-for-backend
-이것이 취업을 위한 백엔드 개발이다 with 자바 (이준형, 2024) 실습
